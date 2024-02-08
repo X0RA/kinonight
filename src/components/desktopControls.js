295fcf55433905e7e4985042bcdba0035c527792
X0RA <annand.rodney@gmail.com>
2024-02-08 12:53:44 +1100

    rhs tooltips

--- src/components/desktopControls.js
+++ src/components/desktopControls.js
@@ -751,13 +751,22 @@ const DesktopControls = ({ playerRef, progress, logOut, formatTime, clearVideo, 
             </button>
             {Object.keys(rhsButtons).map((key) => {
               return (
-                <button
-                  key={key}
-                  className={rhsButtons[key].className}
-                  onClick={rhsButtons[key].onClick}
-                  disabled={rhsButtons[key].disabled}>
-                  {rhsButtons[key].icon}
-                </button>
+                <>
+                  <button
+                    key={key}
+                    className={rhsButtons[key].className}
+                    onClick={rhsButtons[key].onClick}
+                    disabled={rhsButtons[key].disabled}>
+                    {rhsButtons[key].icon}
+                  </button>
+                  <div
+                    id={`tooltip-${key}`}
+                    role="tooltip"
+                    className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700 bottom-full whitespace-nowrap">
+                    {rhsButtons[key].tooltip ? rhsButtons[key].tooltip : ""}
+                    <div className="tooltip-arrow" data-popper-arrow></div>
+                  </div>
+                </>
               );
             })}
           </div>
@@ -768,24 +777,3 @@ const DesktopControls = ({ playerRef, progress, logOut, formatTime, clearVideo, 
 };
 
 export default DesktopControls;
-// {Object.keys(rhsButtons).map((key) => {
-//   return (
-//     <div key={key} className="relative">
-//       <button
-//         data-tooltip-target={`tooltip-${key}`}
-//         key={key}
-//         className={rhsButtons[key].className}
-//         onClick={rhsButtons[key].onClick}
-//         disabled={rhsButtons[key].disabled}>
-//         {rhsButtons[key].icon}
-//       </button>
-      // <div
-      //   id={`tooltip-${key}`}
-      //   role="tooltip"
-      //   className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700 bottom-full whitespace-nowrap">
-      //   {rhsButtons[key].tooltip ? rhsButtons[key].tooltip : ""}
-      //   <div className="tooltip-arrow" data-popper-arrow></div>
-      // </div>
-//     </div>
-//   );
-// })}
